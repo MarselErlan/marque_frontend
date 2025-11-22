305a26428a1806deb4be03a4a68777cd11087c35
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-11-22 16:22:06 -0600

    workin on top bar icon 3

--- components/SparklesIcon.tsx
+++ components/SparklesIcon.tsx
@@ -17,10 +17,15 @@ export const SparklesIcon = React.forwardRef<SVGSVGElement, SparklesIconProps>(
         className={cn("shrink-0", className)}
         {...props}
       >
-        {/* Smaller sparkle (top left) - four-pointed star like a plus rotated 45 degrees */}
-        <path d="M8 4L8.5 3L9.5 3L10 4L9.5 5L8.5 5L8 4Z" />
-        {/* Larger sparkle (bottom center) - four-pointed diamond star */}
-        <path d="M12 20L14 15L19 13L14 11L12 6L10 11L5 13L10 15L12 20Z" />
+        {/* Smaller sparkle (top left) - four-pointed star with rounded bulbous points */}
+        <path d="M6.5 2.5 C6.5 2.5 7 1.5 8 1.5 C9 1.5 9.5 2.5 9.5 2.5 C9.5 2.5 10.5 2 10.5 3 C10.5 4 9.5 4.5 9.5 4.5 C9.5 4.5 10 5.5 9 5.5 C8 5.5 7.5 4.5 7.5 4.5 C7.5 4.5 6.5 5 6.5 4 C6.5 3 7.5 2.5 7.5 2.5 C7.5 2.5 7 1.5 6.5 2.5 Z" />
+        {/* Central diamond cutout for smaller sparkle */}
+        <path d="M7.5 3.5 L8 3 L8.5 3.5 L8 4 L7.5 3.5 Z" fill="white" />
+        
+        {/* Larger sparkle (bottom right) - four-pointed star with rounded bulbous points */}
+        <path d="M10 17 C10 17 12.5 12.5 17.5 10 C22.5 7.5 25 2.5 25 2.5 C25 2.5 22.5 -2.5 17.5 0 C12.5 2.5 10 7.5 10 7.5 C10 7.5 7.5 2.5 2.5 0 C-2.5 -2.5 -5 2.5 -5 2.5 C-5 2.5 -2.5 7.5 2.5 10 C7.5 12.5 10 17 10 17 Z" />
+        {/* Central diamond cutout for larger sparkle */}
+        <path d="M10 13.5 L11.5 12 L13 13.5 L11.5 15 L10 13.5 Z" fill="white" />
       </svg>
     )
   }
