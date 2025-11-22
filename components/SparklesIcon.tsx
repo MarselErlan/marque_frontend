1b315d12c483a6285b271f6e793747633d406c62
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-11-22 17:38:15 -0600

    workin on top bar icon 10

--- components/SparklesIcon.tsx
+++ components/SparklesIcon.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
+import { Sparkles } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
-interface SparklesIconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+interface SparklesIconProps {
   className?: string
   strokeWidth?: number
 }
 
-export const SparklesIcon = React.forwardRef<HTMLImageElement, SparklesIconProps>(
-  ({ className, strokeWidth = 1.5, ...props }, ref) => {
-    return (
-      <img
-        ref={ref}
-        src="/images/image.svg"
-        alt="Sparkles"
-        className={cn("shrink-0", className)}
-        {...props}
-      />
-    )
-  }
-)
-
-SparklesIcon.displayName = "SparklesIcon"
+export const SparklesIcon: React.FC<SparklesIconProps> = ({ className, strokeWidth = 1.5 }) => {
+  return (
+    <Sparkles
+      className={cn("shrink-0", className)}
+      strokeWidth={strokeWidth}
+    />
+  )
+}
